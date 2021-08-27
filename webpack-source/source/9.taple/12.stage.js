8a702e1d79807ae0089a815f2c204a6d8501d0ef
Lee <lee_227@163.com>
2021-08-27 16:08:20 +0800

    i

--- webpack-source/source/9.taple/12.stage.js
+++ webpack-source/source/9.taple/12.stage.js
@@ -1,17 +1,17 @@
-let { SyncHook } = require('./source/index');
-let hook = new SyncHook(['name']);
-debugger;
-hook.tap({ name: 'tap1', stage: 1 }, (name) => {
-  console.log(1, name);
-});
-hook.tap({ name: 'tap3', stage: 3 }, (name) => {
-  console.log(3, name);
-});
-hook.tap({ name: 'tap5', stage: 5 }, (name) => {
-  console.log(4, name);
-});
-hook.tap({ name: 'tap2', stage: 2 }, (name) => {
-  console.log(2, name);
-});
+let { SyncHook } = require('./source/index')
+let hook = new SyncHook(['name'])
 
-hook.call('lee');
+hook.tap({ name: 'tap1', stage: 1 }, (name) => {
+  console.log(1, name)
+})
+hook.tap({ name: 'tap3', stage: 3 }, (name) => {
+  console.log(3, name)
+})
+hook.tap({ name: 'tap5', stage: 5 }, (name) => {
+  console.log(4, name)
+})
+hook.tap({ name: 'tap2', stage: 2 }, (name) => {
+  console.log(2, name)
+})
+
+hook.call('lee')
