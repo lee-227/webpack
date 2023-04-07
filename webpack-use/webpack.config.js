b27093f12a65d1002a8fccf10b297b2cfe41f0ce
Xiao Lee <lee_227@163.com>
2023-04-07 22:09:20 +0800

    fea(webpack): resolve

--- webpack-use/webpack.config.js
+++ webpack-use/webpack.config.js
@@ -32,6 +32,12 @@ module.exports = (env, argv) => {
       // publicPath: '/lee', // 引用资源路径要加的前缀
       // 不清楚具体路径时可以留空，然后再应用的入口文件设置__webpack_public_path__，实现在运行时进行动态设置
     },
+    resolve: {
+      extensions: ['.js', '.css', '.json'], // 导入模块没添加后缀时尝试请求的后缀
+      alias: {
+        '@': path.resolve(__dirname, 'src'), // 别名 导入 @ 开头路径模块时在 src 下
+      }
+    },
     // devServer 会启动一个 HTTP 开发服务器，把一个文件夹作为静态根目录
     // 为了提高性能，使用的内存文件系统
     // 默认情况下 devServer 会读取打包后的路径
